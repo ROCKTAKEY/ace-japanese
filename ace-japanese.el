719993b87cf0c1ecca885cda95f5782556edd2fa
ROCKTAKEY <rocktakey@gmail.com>
2020-12-21 11:26:47 +0900

    Initialize.

---
+++ ace-japanese.el
@@ -0,0 +1,32 @@
+;;; ace-japanese.el --- Jump Japanese character on ace-jump-mode  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  ROCKTAKEY
+
+;; Author: ROCKTAKEY <rocktakey@gmail.com>
+;; Keywords: extensions
+;; URL: https://github.com/ROCKTAKEY/ace-japanese
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "24.1"))
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+
+(provide 'ace-japanese)
+;;; ace-japanese.el ends here
